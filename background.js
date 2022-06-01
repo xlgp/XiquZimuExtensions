450e9258bf6486f7c54fbaf8e5ab65723020d5af
hzsn-csh <hzsn-csh@qq.com>
2022-06-01 10:13:44 +0800

    新增icon

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 (function () {
 	'use strict';
 
-	chrome.runtime.onInstalled.addListener(e=>{console.log(e);});chrome.action.onClicked.addListener(e=>{console.log(e),e.id&&chrome.tabs.sendMessage(e.id,{key:"create",value:""});});chrome.runtime.onMessage.addListener((e,o,n)=>{console.log(e);});
+	chrome.runtime.onInstalled.addListener(e=>{console.log(e);});chrome.action.onClicked.addListener(e=>{console.log(e),e.id&&chrome.tabs.sendMessage(e.id,{key:"create",value:""});});chrome.runtime.onMessage.addListener((e,n,o)=>{console.log(e),o(e);});
 
 })();
